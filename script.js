1e34ddc60ca674ada9b2ce6f03ce95fe7f200090
Tech-A18 <Tech-A18@tech-a18s-air.home>
2017-11-04 23:09:32 +0000

    I just need to add winning conditions

--- script.js
+++ script.js
@@ -2,26 +2,45 @@ $(function(event) {
 
 var counter= 0;
 
+var combinationsArrayForO = []
+
+var combinationsArrayForX = []
+
 function setBoxesListeners() {
 
 	$('.boxes').click(function(){
 
+
 		if($(this).html() == ''){
 		counter = turnDisplay();
-		console.log(counter);
+		// console.log(counter);
 		if (counter%2 == 0) {
-			$(this).html('O')
-			$('#turn').html('X')} else{
-				$(this).html('X')
-				$('#turn').html('O')}
+			$(this).html('O');
+			$('#turn').html('X');
+			combinationsArrayForO.push($(this).attr('data-num'));
+			console.log("player0 " + combinationsArrayForO)
+
+		} else {
+				$(this).html('X');
+				$('#turn').html('O');
+				combinationsArrayForX.push($(this).attr('data-num'));
+				console.log("playerX " + combinationsArrayForX)
+			};
 		}
+
+
 	})
+
 }
 
 function resetBoardListener(){
 	$('button').click(function(){
 
 		 $(".boxes").html('')
+		 combinationsArrayForO = []
+
+		 combinationsArrayForX = []
+
 	})
 }
 
@@ -29,28 +48,22 @@ function turnDisplay(){
 		return counter += 1;
 }
 
+function checkWinningCondition(){
 
+	if (combinationsArrayForX == [0,3,6]){
+		alert("player X has won")
+	}
+
+
+
+
+}
 
 setBoxesListeners();
 
 resetBoardListener();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+checkWinningCondition()
 
 
 
