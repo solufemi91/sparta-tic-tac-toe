42030c9dbee14cc7cecc482eb4aaab4daebdb912
Tech-A18 <Tech-A18@tech-a18s-air.home>
2017-11-04 20:35:02 +0000

    perfected same value from being inputed twice

--- script.js
+++ script.js
@@ -2,33 +2,19 @@ $(function(event) {
 
 var counter= 0;
 
-var $arrayOfClickedBoxes = []
-
-function setBoxesListeners(){
+function setBoxesListeners() {
 
 	$('.boxes').click(function(){
-		var counter = turnDisplay()
-		console.log(counter)
+
+		if($(this).html() == ''){
+		counter = turnDisplay();
+		console.log(counter);
 		if (counter%2 == 0) {
-			if($(this).html() == ''){
 			$(this).html('O')
-			$('#turn').html('X')
-			$arrayOfClickedBoxes.push($(this).attr('data-num'))
-			console.log($arrayOfClickedBoxes)}
-
-		} else { // if the thing that is clicked is not in the array, excute block of code
-			// or if the thing that is clicked is empty, execute code
-			if($(this).html() == ''){
-			$(this).html('X')
-			$('#turn').html('O')
-			$arrayOfClickedBoxes.push($(this).attr('data-num'))
-			console.log($arrayOfClickedBoxes)}
-
-
-
+			$('#turn').html('X')} else{
+				$(this).html('X')
+				$('#turn').html('O')}
 		}
-
-
 	})
 }
 
@@ -43,12 +29,7 @@ function turnDisplay(){
 		return counter += 1;
 }
 
-//make sure that if a box is clicked it cannot be clicked again
 
-// once a box has been clicked, take note of that particular box
-
-// how are the boxes distinguished?
-//the data-num attribute
 
 setBoxesListeners();
 
